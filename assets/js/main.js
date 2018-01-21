ad1e2c775ba44a3ddffaa755486ac611e8a8640b
viococo <violaine.bodolec@gmail.com>
2018-01-21 16:51:34 +0100

    animation home

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,23 @@
-var txt
+var project_txt = $('[data-project]')
+project_txt.fadeOut(0)
+
 $(document).ready(function() {
 	$('#fullpage .img').fullpage({
 		navigation: true,
 		onLeave: function(index, nextIndex, direction){
 			var leavingSection = $(this);
-			console.log(nextIndex)
-			//after leaving section 2 
+			var id = nextIndex - 1
+			var nextSection = $(project_txt[id])
+			project_txt.fadeOut(300)
+			setTimeout(function(){
+				console.log(id)
+				if(id > 0)
+					$('.txtArea').addClass('project')
+				else
+					$('.txtArea').removeClass('project')
+				
+				nextSection.fadeIn()
+			}, 300);			
 		}
 	});
 });
